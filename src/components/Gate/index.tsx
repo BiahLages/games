0f96f85985b77401169b2487e12c535ed90af275
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-11-03 21:10:03 -0300

    Login page implementation

---
+++ src/components/Gate/index.tsx
@@ -0,0 +1,138 @@
+import { error, isPw, validateEmail, validateName, validatePassword } from "../../utils/validation.tools";
+import { BackgroundForm, SubmitButtom, SwicherContainer, SwicherButtom, ContainerVerification, VerificationResponse } from "./styles";
+import { DataType } from "../../types/interfaces/users";
+import { useAuth } from "../../contexts/AccountContext";
+import { useEffect, useState } from "react";
+import { api } from "../../helpers/Api";
+import Input from "../Input";
+
+const Gate = (): JSX.Element => {
+	const { login } = useAuth();
+
+	const [mode, setMode] = useState(true);
+	const [valueName, setValueName] = useState("");
+	const [valueEmail, setValueEmail] = useState("");
+	const [valuePassword, setValuePassword] = useState("");
+	const [validPasswordCharacters, setValidPasswordCharacters] = useState(false);
+	const [validPasswordLength, setValidPasswordLength] = useState(false);
+
+	const action = async (): Promise<void> => {
+		const isValidEmail = validateEmail(valueEmail);
+		const isValidPassword = validatePassword(valuePassword);
+
+		if (isValidEmail && isValidPassword) {
+			const data: DataType = {
+				email: valueEmail,
+				password: valuePassword,
+			};
+
+			switch (mode) {
+				case false:
+					const isValidName = validateName(valueName);
+					if (isValidName) {
+						data.name = valueName;
+						data.isAdmin = false;
+						const register = await api.post(`/users`, data).then(res => res);
+						switch (register.status) {
+							case 201:
+								delete data.name;
+								const loginAfterRegister = await api.post(`/auth/login`, data).then(res => res);
+								switch (loginAfterRegister.status) {
+									case 200:
+										login(loginAfterRegister.data);
+										break;
+
+									default:
+										error("couldn't login after register");
+										break;
+								}
+								break;
+
+							default:
+								error("couldn't register");
+								break;
+						}
+					}
+					break;
+
+				case true:
+					try {
+						const sigin = await api.post(`/auth/login`, data).then(res => res);
+						switch (sigin.status) {
+							case 200:
+								login(sigin.data);
+								break;
+
+							default:
+								error("Try again with correct credentials");
+								break;
+						}
+					} catch (err) {
+						error("Try again with correct credentials");
+					}
+					break;
+			}
+		}
+	};
+
+	useEffect(() => {
+		setValidPasswordCharacters(isPw.test(valuePassword));
+		setValidPasswordLength(valuePassword.length > 7);
+	}, [valuePassword]);
+
+	return (
+		<BackgroundForm>
+			<h1>{!mode ? `Register` : `SignIn`}</h1>
+			<form name="Gate">
+				<div>
+					{!mode && (
+						<Input
+							label="name"
+							placeholder="User Name"
+							type="text"
+							value={setValueName}
+						/>
+					)}
+					<Input
+						label="email"
+						placeholder="username2022@email.com"
+						type="email"
+						value={setValueEmail}
+					/>
+					<Input
+						label="password"
+						placeholder="********"
+						type="password"
+						value={setValuePassword}
+					/>
+					<ContainerVerification>
+						<VerificationResponse>{validPasswordLength ? "✅" : "⛔️"} 8 characters</VerificationResponse>
+						<VerificationResponse>{validPasswordCharacters ? "✅" : "⛔️"} Uppercase | Lowercase | Symbol | Number</VerificationResponse>
+					</ContainerVerification>
+				</div>
+				<SubmitButtom
+					onClick={(e): void => {
+						action();
+						e.preventDefault();
+						e.stopPropagation();
+					}}
+				>
+					{!mode ? `Register` : `SignIn`}
+				</SubmitButtom>
+			</form>
+			<SwicherContainer>
+				<p>{mode ? `Don't have an account?` : `Alread have an account?`}</p>
+
+				<SwicherButtom
+					onClick={(): void => {
+						setMode(!mode);
+					}}
+				>
+					{mode ? `Register` : `SignIn`}
+				</SwicherButtom>
+			</SwicherContainer>
+		</BackgroundForm>
+	);
+};
+
+export default Gate;
---
+++ src/components/Gate/styles.ts
@@ -0,0 +1,90 @@
+import styled, { keyframes } from "styled-components";
+import mixings from "../../assets/styles/mixins";
+
+const toRight = keyframes`
+  0% {
+    position: relative;
+    left: -50vw;
+    opacity: 0;
+  }
+ 100% {
+  position: relative;
+  left: 0px;
+  opacity: 1;
+ }`;
+
+export const BackgroundForm = styled.div`
+	height: auto;
+	width: 75rem;
+	padding: 1em;
+	background: #252525;
+	border-radius: 1em;
+	box-shadow: 1px 1px 4px 1px ${mixings.colors.primaryColor};
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-content: center;
+	align-items: center;
+	animation: ${toRight} 2s normal;
+
+	h1 {
+		font-size: 4em;
+	}
+`;
+
+export const SwicherContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	p {
+		font-size: 2.5em;
+	}
+`;
+
+export const SwicherButtom = styled.button`
+	height: 2.5em;
+	width: 5.5em;
+	background: transparent;
+	color: ${mixings.colors.primaryColor};
+	border: none;
+	border-radius: 0.5em;
+	font-family: ${mixings.constants.FontFamily};
+	font-size: 2.5em;
+	cursor: pointer;
+
+	&:hover {
+		transform: scale(0.98);
+	}
+`;
+
+export const SubmitButtom = styled.button`
+	position: relative;
+	left: calc(50% - 3em);
+	height: 2em;
+	width: 6em;
+	background: ${mixings.colors.primaryColor};
+	color: ${mixings.colors.contrast1};
+	border: none;
+	border-radius: 2em;
+	margin-top: 1em;
+	font-family: ${mixings.constants.FontFamily};
+	font-size: 16px;
+	cursor: pointer;
+
+	&:hover {
+		transform: scale(0.98);
+	}
+`;
+
+export const ContainerVerification = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+export const VerificationResponse = styled.span`
+	font-size: 2.5em;
+	font-style: italic;
+	margin: 0 1em 1em 1.5em;
+`;
