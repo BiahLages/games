2bcd701ee639635aaf16e516d7ec21db65dbe7b5
maxxcleiton <maxcleiton@live.com>
2022-11-04 20:44:35 -0300

    update Card - adicao de styles e informacoes

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,10 +1,13 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import {STitle, SDate, SDescription, SActionButton} from './styles';
 
-const Card = () => (
-    <div>
-        
-    </div>
-);
-    
-export default Card
+const Card = (props: any) =>
+<div>
+	<STitle>Titulo!</STitle>
+	<SDate>2019</SDate>
+	<SDescription>Integer iaculis vestibulum malesuada. Maecenas accumsan eleifend mi, non tempor massa elementum vel. Vivamus condimentum at orci ultricies aliquam. Etiam gravida ullamcorper metus, in suscipit lacus sagittis ultrices.</SDescription>
+	<SActionButton>Gender1</SActionButton>
+	<SActionButton>Gender2</SActionButton>
+</div>;
+
+export default Card;
