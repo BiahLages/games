d4d503d461a098131e434f164faff73c3bc5c734
malkavianson <63012624+Malkavianson@users.noreply.github.com>
2022-11-05 00:42:44 -0300

    Feat: Profiles

--- src/components/Card/styles.tsx
+++ src/components/Card/styles.tsx
@@ -1,28 +1,28 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const STitle = styled.h2`
-  color: #fff;
-  font-weight: 300;
-`
+	color: #fff;
+	font-weight: 300;
+`;
 export const SDate = styled.div`
-  color: #ccc;
-  font-weight: 300;
-  margin: 6px 0;
-`
+	color: #ccc;
+	font-weight: 300;
+	margin: 6px 0;
+`;
 export const SDescription = styled.p`
-  color: #fff;
-  font-weight: 300;
-`
+	color: #fff;
+	font-weight: 300;
+`;
 export const SActionButton = styled.button`
-  margin: 0 5px;
-  padding: 8px 14px;
-  background: rgba(155, 155, 155, 0.2);
-  color: #fff;
-  cursor: pointer;
-  border: 1px solid #fff;
-  outline: 0;
-  font-weight: 300;
-  :hover {
-    opacity: 0.8;
-  }
-`
+	margin: 0 5px;
+	padding: 8px 14px;
+	background: rgba(155, 155, 155, 0.2);
+	color: #fff;
+	cursor: pointer;
+	border: 1px solid #fff;
+	outline: 0;
+	font-weight: 300;
+	:hover {
+		opacity: 0.8;
+	}
+`;
