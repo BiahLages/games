4a48fe22974f38e2027a61e07b18a6c658e931f1
BiahLages <biahlages@gmail.com>
2022-11-06 14:54:37 -0300

    fix edit user

--- src/components/MenuUpdateUser/index.tsx
+++ src/components/MenuUpdateUser/index.tsx
@@ -4,182 +4,58 @@ import { BackgroundForm, SubmitButton, ContainerVerification, VerificationRespon
 // import { DataType } from "../../types/interfaces/users";
 // import { useAuth } from "../../contexts/AccountContext";
 import { useEffect, useState } from "react";
+import { useConfigUser } from "src/contexts/ConfigUserContext";
 // import { api } from "../../helpers/Api";
 
-// const Gate = (): JSX.Element => {
-// 	const { login } = useAuth();
-
-// 	const [mode, setMode] = useState(true);
-
-// 			switch (mode) {
-// 				case false:
-// 					const isValidName = validateName(valueName);
-// 					if (isValidName) {
-// 						data.name = valueName;
-// 						data.isAdmin = false;
-// 						const register = await api.post(`/users`, data).then(res => res);
-// 						switch (register.status) {
-// 							case 201:
-// 								delete data.name;
-// 								const loginAfterRegister = await api.post(`/auth/login`, data).then(res => res);
-// 								switch (loginAfterRegister.status) {
-// 									case 200:
-// 										login(loginAfterRegister.data);
-// 										break;
-
-// 									default:
-// 										error("couldn't login after register");
-// 										break;
-// 								}
-// 								break;
-
-// 							default:
-// 								error("couldn't register");
-// 								break;
-// 						}
-// 					}
-// 					break;
-
-// 				case true:
-// 					try {
-// 						const sigin = await api.post(`/auth/login`, data).then(res => res);
-// 						switch (sigin.status) {
-// 							case 200:
-// 								login(sigin.data);
-// 								break;
-
-// 							default:
-// 								error("Try again with correct credentials");
-// 								break;
-// 						}
-// 					} catch (err) {
-// 						error("Try again with correct credentials");
-// 					}
-// 					break;
-// 			}
-// 		}
-// 	};
-
-// 	return (
-
-// 	);
-// };
-
 const MenuUpdateUser = () => {
-	const [valueName, setValueName] = useState("");
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const [valueEmail, setValueEmail] = useState("");
-	const [valuePassword, setValuePassword] = useState("");
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const [valueCPF, setValueCPF] = useState("");
-	const [validPasswordCharacters, setValidPasswordCharacters] = useState(false);
-	const [validPasswordLength, setValidPasswordLength] = useState(false);
+	const { states, setStates, functions } = useConfigUser();
 
-	// const action = async (): Promise<void> => {
-	// 	const isValidEmail = validateEmail(valueEmail);
-	// 	const isValidPassword = validatePassword(valuePassword);
-
-	// 	if (isValidEmail && isValidPassword) {
-	// 		const data: DataType = {
-	// 			email: valueEmail,
-	// 			password: valuePassword,
-	// 			cpf: valueCPF,
-	// 		};
-
-	// 		switch (mode) {
-	// 			case false:
-	// 				const isValidName = validateName(valueName);
-	// 				if (isValidName) {
-	// 					data.name = valueName;
-	// 					data.isAdmin = false;
-	// 					const register = await api.post(`/users`, data).then(res => res);
-	// 					switch (register.status) {
-	// 						case 201:
-	// 							delete data.name;
-	// 							const loginAfterRegister = await api.post(`/auth/login`, data).then(res => res);
-	// 							switch (loginAfterRegister.status) {
-	// 								case 200:
-	// 									login(loginAfterRegister.data);
-	// 									break;
-
-	// 								default:
-	// 									error("couldn't login after register");
-	// 									break;
-	// 							}
-	// 							break;
-
-	// 						default:
-	// 							error("couldn't register");
-	// 							break;
-	// 					}
-	// 				}
-	// 				break;
-
-	// 			case true:
-	// 				try {
-	// 					const sigin = await api.post(`/auth/login`, data).then(res => res);
-	// 					switch (sigin.status) {
-	// 						case 200:
-	// 							login(sigin.data);
-	// 							break;
-
-	// 						default:
-	// 							error("Try again with correct credentials");
-	// 							break;
-	// 					}
-	// 				} catch (err) {
-	// 					error("Try again with correct credentials");
-	// 				}
-	// 				break;
-	// 		}
-	// 	}
-	// }
-
-	useEffect(() => {
-		setValidPasswordCharacters(isPw.test(valuePassword));
-		setValidPasswordLength(valuePassword.length > 7);
-	}, [valuePassword]);
+	// useEffect(() => {
+	// 	setValidPasswordCharacters(isPw.test(valuePassword));
+	// 	setValidPasswordLength(valuePassword.length > 7);
+	// }, [valuePassword]);
 
 	return (
 		<BackgroundForm>
 			<h1>Edit Info</h1>
 			<form name="Edit">
 				<div>
-					<Input
-						label="name"
+					<input
 						placeholder="User Name"
 						type="text"
-						value={setValueName}
+						value={states.name}
+						onChange={(e: any) => setStates.setName(e.target.value)}
 					/>
-					<Input
-						label="CPF"
+					<input
+						// label="CPF"
 						placeholder="01234567890"
 						type="text"
-						value={setValueCPF}
+						value={states.cpf}
+						onChange={(e: any) => setStates.setCpf(e.target.value)}
 					/>
 
-					<Input
-						label="email"
+					<input
+						// label="email"
 						placeholder="username2022@email.com"
 						type="email"
-						value={setValueEmail}
+						value={states.email}
+						onChange={(e: any) => setStates.setEmail(e.target.value)}
 					/>
-					<Input
-						label="password"
+					<input
+						// label="password"
 						placeholder="********"
 						type="password"
-						value={setValuePassword}
+						value=""
+						onChange={(e: any) => setStates.setPassword(e.target.value)}
 					/>
-					<ContainerVerification>
+					{/* <ContainerVerification>
 						<VerificationResponse>{validPasswordLength ? "✅" : "⛔️"} 8 characters</VerificationResponse>
 						<VerificationResponse>{validPasswordCharacters ? "✅" : "⛔️"} Uppercase | Lowercase | Symbol | Number</VerificationResponse>
-					</ContainerVerification>
+					</ContainerVerification> */}
 				</div>
 				<SubmitButton
-					onClick={(e): void => {
-						// action();
-						e.preventDefault();
-						e.stopPropagation();
+					onClick={(): void => {
+						functions.handdleUpdateUser();
 					}}
 				/>
 			</form>
