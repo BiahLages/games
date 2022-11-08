9a1101d472cd12a544f11ea1683091dfe66fd7fa
maxxcleiton <maxcleiton@live.com>
2022-11-07 23:50:08 -0300

    updates - ranking & img sizes changing (px to rem)

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,9 +1,13 @@
 import { ICardGames } from "../../types/interfaces/games";
 import { Link } from "react-router-dom";
-
-import { STitle, SDate, SGameImg, /* SDescription, SActionButton,*/ SCard } from "./styles";
+import { IStyleScore } from "src/types/interfaces/games";
+import { STitle, SScore, SStars, SText, SGameImg, /* SDescription, SActionButton, SDate*/ SCard } from "./styles";
 
 const Card = ({ game, currentKey }: { game: ICardGames; currentKey: number }): JSX.Element => {
+	interface IStyleScore {
+		score: number;
+	}
+
 	return (
 		<Link to={`/game/${game.id}`}>
 			<SCard>
@@ -15,6 +19,15 @@ const Card = ({ game, currentKey }: { game: ICardGames; currentKey: number }): J
 					{game.title} - {game.year}
 					{/* <SDate key={`date${currentKey}`}>{game.year}</SDate> */}
 				</STitle>
+				<SScore>
+					<SStars score={Number(game.score)}>
+						<img
+							src="https://media.discordapp.net/attachments/985645895779508254/1039019480359112775/stars.png"
+							alt="stars imdb"
+						/>
+					</SStars>
+					<SText>{game.score}</SText>
+				</SScore>
 				{/* <SDescription>Integer iaculis vestibulum malesuada. Maecenas accumsan eleifend mi, non tempor massa elementum vel. Vivamus condimentum at orci ultricies aliquam. Etiam gravida ullamcorper metus, in suscipit lacus sagittis ultrices.</SDescription>
 			<SActionButton>Gender1</SActionButton>
 			<SActionButton>Gender2</SActionButton> */}
